55db520aa52cc0ede5010b2ab7d3a8a28f9d0286
jrmedd <hello@jamesmedd.co.uk>
2026-05-17 11:32:15 +0100

    Complete RSVP form

--- src/components/FormInputs.jsx
+++ src/components/FormInputs.jsx
@@ -1,34 +1,152 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
 
-const StyledLabel = styled.label(props => css`
-  display: block;
-  font-family: ${props => props.theme.fonts.body};
-  color: ${props => props.theme.text.body};
-  font-size: 1.25rem;
-  margin-bottom: 1rem;
-`)
+const StyledLabel = styled.label(
+  (props) => css`
+    display: block;
+    font-family: ${(props) => props.theme.fonts.body};
+    color: ${(props) => props.theme.text.body};
+    font-size: 1.25rem;
+  `,
+);
 
-const StyledInput = styled.input(props => css`
-  display: block;
-  width: 100%;
-  padding: .5rem;
-  font-size: 1rem;
-  font-family: ${props => props.theme.fonts.body};
-  color: ${props => props.theme.text.body};
-  background: none;
-  border: none;
-  border-radius: .25rem;
-  box-shadow: 0 0 0 1px ${props => props.theme.text.body};
-  outline: none;
-  transition: box-shadow 0.2s ease;
-  &:active, &:focus {
-    box-shadow: 0 0 0 2px ${props => props.theme.text.body};
-  }
-`)
+const StyledInput = styled.input(
+  (props) => css`
+    display: block;
+    width: 100%;
+    padding: 0.5rem 1rem;
+    font-size: 1.25rem;
+    font-family: ${(props) => props.theme.fonts.body};
+    color: ${(props) => props.theme.text.body};
+    background: none;
+    border: none;
+    border-radius: 0.25rem;
+    box-shadow: 0 0 0 1px ${(props) => props.theme.text.body};
+    outline: none;
+    transition: box-shadow 0.2s ease;
+    &:active,
+    &:focus {
+      box-shadow: 0 0 0 2px ${(props) => props.theme.text.body};
+    }
+  `,
+);
 
-export const FormInput = props => (
-  <StyledLabel>
-    {props.label}
-    <StyledInput type={props.type} name={props.name} value={props.value} onChange={props.onChange} />
-  </StyledLabel>
-) 
+const StyledFieldset = styled.fieldset(
+  (props) => css`
+    border: none;
+    padding: 0;
+    margin: 0;
+  `,
+);
+
+const StyledLegend = styled.legend(
+  (props) => css`
+    font-family: ${(props) => props.theme.fonts.body};
+    color: ${(props) => props.theme.text.body};
+    font-size: 1.5rem;
+    margin-bottom: 0.5rem;
+  `,
+);
+
+const StyledSelector = styled.div(
+  (props) => css`
+    display: flex;
+    flex-direction: row;
+    gap: -1px;
+  `,
+);
+
+const StyledRadioGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledRadioLabel = styled.label(
+  (props) => css`
+    display: flex;
+    align-items: center;
+    font-family: ${(props) => props.theme.fonts.body};
+    color: ${(props) => props.theme.text.body};
+    font-size: 1.25rem;
+    box-shadow: 0 0 0 1px ${(props) => props.theme.text.body};
+    padding: .75rem 1.5rem;
+    border-radius: 0.25rem;
+    cursor: pointer;
+    background-color: ${(props) => props.theme.background};
+    background-image: linear-gradient(
+      ${(props) => props.theme.interactive},
+      ${(props) => props.theme.interactive}
+    );
+    background-repeat: no-repeat;
+    background-position: left top;
+    background-size: 0% 100%;
+    transition:
+      background-size 0.25s ease,
+      color 0.2s ease,
+      box-shadow 0.2s ease;
+    &:hover, &:focus  {
+      box-shadow: 0 0 0 2px ${(props) => props.theme.text.body};
+    }
+    input[type="radio"]:checked ~ & {
+      background-size: 100% 100%;
+      color: ${(props) => props.theme.background};
+      box-shadow: 0 0 0 1px ${(props) => props.theme.interactive};
+    }
+    ${StyledRadioGroup}:first-child & {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+      background-position: right top;
+    }
+    ${StyledRadioGroup}:last-child & {
+      border-top-left-radius: 0; 
+      border-bottom-left-radius: 0;
+      background-position: left top;
+    }
+  `,
+);
+
+const StyledRadio = styled.input.attrs({ type: "radio" })`
+  opacity: 0;
+  width: 0;
+  height: 0;
+  position: absolute;
+  pointer-events: none;
+`;
+
+export const TwoAnswerQuestion = (props) => (
+  <StyledFieldset>
+    <StyledLegend>{props.label}</StyledLegend>
+    <StyledSelector>
+      {props.options.map((option) => (
+        <StyledRadioGroup key={option.value}>
+          <StyledRadio
+            id={`${props.name}-${option.value}`}
+            name={props.name}
+            value={option.value}
+            onChange={props.onChange}
+          />
+          <StyledRadioLabel
+            htmlFor={`${props.name}-${option.value}`}
+            key={option.value}
+          >
+            {option.label}
+          </StyledRadioLabel>
+        </StyledRadioGroup>
+      ))}
+    </StyledSelector>
+  </StyledFieldset>
+);
+
+export const FormInput = (props) => (
+  <>
+    <StyledLabel htmlFor={props.name}>{props.label}</StyledLabel>
+    <StyledInput
+      id={props.name}
+      type={props.type}
+      name={props.name}
+      value={props.value}
+      onChange={props.onChange}
+      autoComplete={props.autoComplete}
+      inputMode={props.inputMode}
+    />
+  </>
+);
--- src/routes/rsvp.jsx
+++ src/routes/rsvp.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { Stack } from "../components/Layout"
 import { Heading, Paragraph, ScreenReaderOnly } from "../components/Typography"
 import { Button } from "../components/Button"
-import { FormInput } from "../components/FormInputs"
+import { FormInput, TwoAnswerQuestion } from "../components/FormInputs"
 
 const StyledForm = styled.form`
   display: flex;
@@ -20,12 +20,19 @@ export const Component = props => {
     <Heading $size='l' id='career'>
       RSVP
     </Heading>
-    <StyledForm action="POST">
-      <FormInput name="name" label="Name(s)" type="text" />
-      <FormInput name="email" label="Email address" type="email" />
-      <FormInput name="attending" label="Will you be attending?" type="text" />
-      <FormInput name="guests" label="How many guests (including yourself)?" type="number" />
-      <FormInput name="dietaryRequirements" label="Dietary requirements (if any)" type="text" />
+    <StyledForm>
+      <FormInput autoComplete='name' name="name" label="Name(s)" type="text" />
+      <FormInput autoComplete='email' name="email" label="Email address" type="email" />
+      <TwoAnswerQuestion name="attending" label="Will you be attending?" options={[
+        { label: 'Yes',  value: 'yes' },
+        { label: 'No', value: 'no' }
+      ]} />
+      <TwoAnswerQuestion name="songRequest" label="Are you a 12PM or a 6PM guest?" options={[
+        { label: '12PM',  value: '12pm' },
+        { label: '6PM', value: '6pm' }
+      ]} />
+      <FormInput name="guests" label="How many guests (including yourself)?" type="text" inputMode="numeric" />
+      <FormInput name="dietaryRequirements" label="Dietary requirements" type="text" />
       <Button as='button' type="submit">Submit</Button>
     </StyledForm>
   </Stack>
